5862a2f0c69c0f6db0e6033b3b1e9b33f91a3764
xgh <xugenhua@tongyunbao.cn>
2022-07-26 11:25:14 +0800

    feat：链表

---
+++ src/main/java/com/xgh/test/algorithms/LinkedList.java
@@ -0,0 +1,164 @@
+package com.xgh.test.algorithms;
+
+
+public class LinkedList<E> implements List<E> {
+
+    transient int size = 0;
+
+    transient Node<E> first;
+
+    transient Node<E> last;
+
+    public LinkedList() {
+
+
+    }
+
+    void linkFirst(E e) {
+        final Node<E> f = first;
+        final Node<E> newNode = new Node<>(null, e, f);
+        first = newNode;
+        if (f == null) {
+            last = newNode;
+        } else {
+            f.prev = newNode;
+        }
+        size++;
+    }
+
+    void linkLast(E e) {
+        final Node<E> l = last;
+        final Node<E> newNode = new Node<>(l, e, null);
+        last = newNode;
+        if (l == null) {
+            first = newNode;
+        } else {
+            l.next = newNode;
+        }
+        size++;
+    }
+
+    @Override
+    public boolean add(E e) {
+        linkLast(e);
+        return true;
+    }
+
+    @Override
+    public boolean addFirst(E e) {
+        linkFirst(e);
+        return true;
+    }
+
+    @Override
+    public boolean addLast(E e) {
+        linkLast(e);
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        if (o == null) {
+            for (Node<E> x = first; x != null; x = x.next) {
+                if (x.item == null) {
+                    unLink(x);
+                    return true;
+                }
+            }
+        } else {
+            for (Node<E> x = first; x != null; x = x.next) {
+                if (o.equals(x.item)) {
+                    unLink(x);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public E get(int index) {
+        return node(index).item;
+    }
+
+
+    @Override
+    public void printLinkList() {
+        if (this.size == 0) {
+            System.out.println("链表为空");
+        } else {
+            Node<E> temp = first;
+            System.out.print("目前的列表，头节点：" + first.item + " 尾节点：" + last.item + " 整体：");
+            while (temp != null) {
+                System.out.print(temp.item + "，");
+                temp = temp.next;
+            }
+            System.out.println();
+        }
+    }
+
+    E unLink(Node<E> x) {
+        final E element = x.item;
+        final Node<E> prev = x.prev;
+        final Node<E> next = x.next;
+        if (prev == null) {
+            first = next;
+        } else {
+            prev.next = next;
+            x.prev = null;
+        }
+        if (next == null) {
+            last = prev;
+        } else {
+            next.prev = prev;
+            x.next = null;
+        }
+        x.item = null;
+        size--;
+        return element;
+    }
+
+    private Node<E> node(int index) {
+        Node<E> x;
+        if (index < (size >> 1)) {
+            x = first;
+            for (int i = 0; i < index; i++) {
+                x = x.next;
+            }
+        } else {
+            x = last;
+            for (int i = size - 1; i > index; i--) {
+                x = x.prev;
+            }
+        }
+        return x;
+    }
+
+    private static class Node<E> {
+        E item;
+        Node<E> next;
+        Node<E> prev;
+
+        public Node(Node<E> prev, E item, Node<E> next) {
+            this.item = item;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    public static void main(String[] args) {
+        List<String> list = new LinkedList<>();
+        // 添加元素
+        list.add("a");
+        list.addFirst("b");
+        list.addLast("c");
+        // 打印列表
+        list.printLinkList();
+        // 头插元素
+        list.addFirst("d");
+        // 删除元素
+        list.remove("b");
+        // 打印列表
+        list.printLinkList();
+    }
+}
---
+++ src/main/java/com/xgh/test/algorithms/List.java
@@ -0,0 +1,18 @@
+package com.xgh.test.algorithms;
+
+public interface List<E> {
+
+    boolean add(E e);
+
+    boolean addFirst(E e);
+
+    boolean addLast(E e);
+
+    boolean remove(Object o);
+
+    E get(int index);
+
+    void printLinkList();
+
+
+}
